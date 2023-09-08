6c5441e0048536960083facf364144931b6edfe8
AlexandrKataev <dkeyman@mail.ru>
2023-09-08 23:17:32 +0300

    header text

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,5 +1,10 @@
 import styles from './Header.module.scss';
 
 export const Header = () => {
-  return <div className={styles.container}>Test project</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.descr}>Data provided by</div>
+      <div>IEX Cloud</div>
+    </div>
+  );
 };
